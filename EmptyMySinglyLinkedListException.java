9af74ad19ae00a555f68aa093346198a383431dc
Elijah Einstein <30272965+AirbusVsBoeing@users.noreply.github.com>
2017-11-06 15:43:27 -0500

    Add files via upload

---
+++ EmptyMySinglyLinkedListException.java
@@ -0,0 +1,9 @@
+package singlyLinkedList.Construction1;
+
+public class EmptyMySinglyLinkedListException extends RuntimeException{
+	
+	public EmptyMySinglyLinkedListException(String s) {
+		super(s);
+	}
+
+}
