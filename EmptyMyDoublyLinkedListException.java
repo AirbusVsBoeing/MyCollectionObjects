4e6e2d9f25b03221948f75c61f6b2b340607445f
Elijah Einstein <30272965+AirbusVsBoeing@users.noreply.github.com>
2017-11-06 20:48:12 -0500

    Add files via upload

---
+++ EmptyMyDoublyLinkedListException.java
@@ -0,0 +1,10 @@
+package myDoublyLinkedList.Construction1;
+
+//This class is necessary for MyDoublLinkedList
+
+public class EmptyMyDoublyLinkedListException extends RuntimeException {
+
+	public EmptyMyDoublyLinkedListException(String s) {
+		super(s);
+	}
+}
